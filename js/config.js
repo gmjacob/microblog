9f5bf552b32346e9f2d3cbd54fe5947aeebbade5
George M Jacob <george.mammen@springrole.com>
2018-04-14 14:25:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 //Edit the below
 
-var contract = '0x526536ae14314790d82034043eea3ce54313f5e3';
+var contract = '0x86a35979768c2bcd95a8b16cc03cca285ced4528';
 var maxPosts = 5;
-var twitter = 'SwaroopH';
-var facebook = 'SwaroopHegde';
-var github = 'SwaroopH';
+var twitter = 'George';
+var facebook = 'George';
+var github = 'gmjacob';
 var prefix = 'https://alpha-api.ethvigil.com/contract/' + contract;
